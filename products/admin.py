b0c6c0779e23afd1dafbe146cdf58479dbd97033
harshithakurra123 <kurraharshithachowdary@gmail.com>
2025-08-09 16:24:37 +0530

    21625

--- products/admin.py
+++ products/admin.py
@@ -1,11 +1,9 @@
 from django.contrib import admin
-from .models import *
+from .models import Menuitem
 
 
-# Custom Admins
-class ItemAdmin(admin.ModelAdmin):
-    list_display = ['item_name','item_price','created_at']
+
 
 
 # Register your models here.
-admin.site.register(Item,ItemAdmin)
+admin.site.register(MenuItem)
