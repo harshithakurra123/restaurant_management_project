064a6b7de6a549fedee96ac695853fae8c01a955
harshithakurra123 <kurraharshithachowdary@gmail.com>
2025-08-09 16:29:08 +0530

    21625

--- products/admin.py
+++ products/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from .models import Menuitem
 
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display=('name','price')
+    search_fields=('name',)
+
 
 
 
 
 # Register your models here.
-admin.site.register(MenuItem)
+admin.site.register(MenuItem,MenuItemAdmin))
